bf23bcef8d9ec11276e81650dc0298727e3a19bc
BatirDaniel <daniel.batir2003@gmail.com>
2023-07-26 15:12:20 +0300

    Initializing Libro project

---
+++ Libro/Libro.Infrastructure/Persistence/SystemConfigurationService/SystemConfigurationService.cs
@@ -0,0 +1,25 @@
+﻿using Libro.Infrastructure.Persistence.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.Infrastructure.SystemConfiguration
+{
+    public class SystemConfigurationService
+    {
+        public AppSettings? settings { get; private set; }
+
+        public SystemConfigurationService(
+            AppSettings _settings)
+        {
+            settings = _settings;
+        }
+
+        public void SetJWTSecret (string secret)
+        {
+            settings.JWT_Secret = secret;
+        }
+    }
+}
