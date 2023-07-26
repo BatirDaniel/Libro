c6a1639b7e4195a59043774505f9b1faf3da0739
BatirDaniel <daniel.batir2003@gmail.com>
2023-07-26 14:56:16 +0300

    [ent] Adding issue properties

---
+++ Libro/Libro.DataAccess/Entities/Issue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.DataAccess.Entities
+{
+    public class Issue
+    {
+        public string? Id { get; set; }
+
+        public Pos? Pos { get; set; }
+        public string? IdPos { get; set; }
+
+        public IssueTypes? IssueTypes { get; set; }
+        public string? IdType { get; set; }
+
+        public string? IdSubType { get; set; }
+        public string? Priority { get; set; }
+
+        public Status? Status { get; set; }
+        public string? IdStatus { get; set; }
+
+        public string? Memo { get; set; }
+
+        public User? User { get; set; }
+        public string? IdUserCreated { get; set; }
+
+        public User? UserAsigned { get; set; }
+        public string? IdAssigned { get; set; }
+
+        public string? Description { get; set; }
+        public DateTime? AssignedDate { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public DateTime? ModifDate { get; set; }
+        public string? Solution { get; set; }
+
+        public virtual ICollection<Log> Logs { get; set; } = new List<Log>();
+    }
+}
