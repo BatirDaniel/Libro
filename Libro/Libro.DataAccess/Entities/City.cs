596f028e372206f887f183c2a7c8b39334c5d215
BatirDaniel <daniel.batir2003@gmail.com>
2023-07-26 14:49:10 +0300

    [ent] Adding city properties

---
+++ Libro/Libro.DataAccess/Entities/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.DataAccess.Entities
+{
+    public class City
+    {
+        public string? Id { get; set; }
+        public string? CityName { get; set; }
+
+        public virtual ICollection<Pos> Pos { get; set; } = new List<Pos>();
+    }
+}
