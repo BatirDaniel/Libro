766f1e90300b2662a200081f449e667f811537a9
BatirDaniel <daniel.batir2003@gmail.com>
2023-07-26 14:52:34 +0300

    [ent] Adding usertypes properties

---
+++ Libro/Libro.DataAccess/Entities/UserTypes.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace Libro.DataAccess.Entities
+{
+    public class UserTypes : IdentityRole
+    {
+        public string? UserType
+        {
+            get => this.Name;
+            set => this.Name = value;
+        }
+
+        public virtual ICollection<User> Users { get; set; } = new List<User>();
+    }
+}
