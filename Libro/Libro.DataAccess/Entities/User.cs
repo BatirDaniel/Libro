9b4011e062d9f1e4065a1d917f80d4b33a0bc248
BatirDaniel <daniel.batir2003@gmail.com>
2023-07-26 14:50:47 +0300

    [ent] Adding user properties

---
+++ Libro/Libro.DataAccess/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace Libro.DataAccess.Entities
+{
+    public class User : IdentityUser
+    {
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+        public string? Telephone { get; set; }
+        public virtual ICollection<Issue> Issues { get; set; } = new List<Issue>();
+        public virtual ICollection<Log> Logs { get; set; } = new List<Log>();
+    }
+}
