64461aee18b1b0ed4033f03c6e80d45894ff6aad
BatirDaniel <daniel.batir2003@gmail.com>
2023-07-26 14:53:32 +0300

    [ent] Adding log properties

---
+++ Libro/Libro.DataAccess/Entities/Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.DataAccess.Entities
+{
+    public class Log
+    {
+        public string? Id { get; set; }
+
+        public Issue? Issue { get; set; }
+        public string? IdIssue { get; set; }
+
+        public User? User { get; set; }
+        public string? IdUser { get; set; }
+
+        public string? Action { get; set; }
+        public string? Notes { get; set; }
+        public DateTime? InsertDate { get; set; }
+    }
+}
