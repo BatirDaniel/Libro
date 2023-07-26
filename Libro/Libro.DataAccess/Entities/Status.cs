db8cf8d1e2a15a79dcd47579112a005b3efc05bc
BatirDaniel <daniel.batir2003@gmail.com>
2023-07-26 14:49:50 +0300

    [ent] Adding status properties

---
+++ Libro/Libro.DataAccess/Entities/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.DataAccess.Entities
+{
+    public class Status
+    {
+        public string? Id { get; set; }
+        public string? Status_Name { get; set; }
+
+        public virtual ICollection<Issue> Issues { get; set; } = new List<Issue>();
+    }
+}
