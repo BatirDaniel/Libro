78d4717892e89850e0343ba8e76577a3aeac25c1
BatirDaniel <daniel.batir2003@gmail.com>
2023-07-26 14:54:56 +0300

    [ent] Adding issuetypes properties

---
+++ Libro/Libro.DataAccess/Entities/IssueTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.DataAccess.Entities
+{
+    public class IssueTypes
+    {
+        public string? Id { get; set; }
+        public string? IssueLevel { get; set; }
+        public string? ParentIssue { get; set; }
+        public string? Name { get; set; }
+        public DateTime? InsertDate { get; set; }
+
+        public virtual ICollection<Issue> Issues { get; set; } = new List<Issue>();
+    }
+}
