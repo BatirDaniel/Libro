05b1cc0f7fcd2f27f9c25c575cae9a0ee9ddd17f
BatirDaniel <daniel.batir2003@gmail.com>
2023-07-26 14:57:01 +0300

    [ent] Adding connectiontypes properties

---
+++ Libro/Libro.DataAccess/Entities/ConnectionTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.DataAccess.Entities
+{
+    public class ConnectionTypes
+    {
+        public string? Id { get; set; }
+        public string? ConnectionType { get; set; }
+
+        public virtual ICollection<Pos> Pos { get; set; } = new List<Pos>();
+    }
+}
