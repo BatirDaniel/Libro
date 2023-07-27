a9e521e14ced0f44715c526208ff1c3598bcff9c
BatirDaniel <daniel.batir2003@gmail.com>
2023-07-27 16:58:57 +0300

    Added controllers with mediator&logger dependency injected

---
+++ Libro/Libro.Presentation/Controllers/Issue/IssueController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Libro.Presentation.Controllers.Issue
+{
+    public class IssueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Libro/Libro.Presentation/Controllers/Pos/PosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Libro.Presentation.Controllers.Pos
+{
+    public class PosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
