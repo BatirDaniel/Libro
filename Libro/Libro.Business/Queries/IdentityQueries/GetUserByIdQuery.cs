76b39a73dcf6ed10630e1c980c22f843193fabee
BatirDaniel <daniel.batir2003@gmail.com>
2023-08-03 18:34:32 +0300

    query: added the MediatR query that will get the user by id

---
+++ Libro/Libro.Business/Queries/IdentityQueries/GetUserByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Libro.Business.Queries.IdentityQueries
+{
+    public class GetUserByIdQuery : IRequest<string>
+    {
+        public string? Id { get; set; }
+
+        public GetUserByIdQuery(string? id)
+        {
+            Id = id;
+        }
+    }
+}
