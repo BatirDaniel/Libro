e58d230c4aa9eeae8dfef2f907932b5aab2f6380
BatirDaniel <daniel.batir2003@gmail.com>
2023-08-03 18:31:37 +0300

    implementing: mappperly for generating object mappings

---
+++ Libro/Libro.Business/Mappers/Mapperly.cs
@@ -0,0 +1,12 @@
+﻿using Libro.Business.Commands.IdentityCommands;
+using Libro.DataAccess.Entities;
+using Riok.Mapperly.Abstractions;
+
+namespace Libro.Infrastructure.Mappers
+{
+    [Mapper]
+    public partial class Mapperly
+    {
+        public partial User Map(AddUserCommand command);
+    }
+}
