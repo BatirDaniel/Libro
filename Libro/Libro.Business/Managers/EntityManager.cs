ec4e125ef3509d7db1cc79f3da7b4e918c33317b
BatirDaniel <daniel.batir2003@gmail.com>
2023-08-03 18:33:19 +0300

    identity entities: added the methods for [CRUD] operations

---
+++ Libro/Libro.Business/Managers/EntityManager.cs
@@ -0,0 +1,22 @@
+﻿using Libro.DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.Business.Managers
+{
+    public class EntityManager
+    {
+        protected UnitOfWork _unitOfWork;
+        protected ClaimsPrincipal _user;
+
+        public EntityManager(UnitOfWork unitOfWork, ClaimsPrincipal user)
+        {
+            _unitOfWork = unitOfWork;
+            _user = user;
+        }
+    }
+}
---
+++ Libro/Libro.Business/Managers/IdentityManager.cs
@@ -0,0 +1,59 @@
+﻿using Libro.Business.Commands.IdentityCommands;
+using Libro.Business.Services;
+using Libro.Business.Validators;
+using Libro.DataAccess.Entities;
+using Libro.DataAccess.Repository;
+using Libro.Infrastructure.Mappers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+
+namespace Libro.Business.Managers
+{
+    public class IdentityManager : EntityManager
+    {
+        public ILogger<IdentityManager> _logger;
+        public UserManager<User> _userManager;
+        public IdentityService _identityService;
+        public Mapperly _mapperly;
+        public IdentityManager(
+            UnitOfWork unitOfWork,
+            ClaimsPrincipal user,
+            ILogger<IdentityManager> logger,
+            UserManager<User> userManager,
+            IdentityService identityService,
+            Mapperly mapperly) : base(unitOfWork, user)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _identityService = identityService;
+            _mapperly = mapperly;
+        }
+
+        public async Task<string> Create(AddUserCommand command)
+        {
+            command.Username = command.Username?.Trim();
+
+            var _validation = new AddUserCommandValidator();
+            var result = _validation.Validate(command);
+
+            if (result != null)
+                return result.ToString();
+
+            User user = _mapperly.Map(command);
+
+            if (user.UserName != null)
+                if (await _userManager.FindByNameAsync(user.UserName) != null)
+                    return "The provided username is already in use";
+
+            if (user.Email != null)
+                if (await _userManager.FindByEmailAsync(user.Email) != null)
+                    return "The provided email is already in use";
+
+            await _unitOfWork.Users.Create(user);
+            await _unitOfWork.Save();
+
+            return null;
+        }
+    }
+}
