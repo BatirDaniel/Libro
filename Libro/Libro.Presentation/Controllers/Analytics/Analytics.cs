7ac9a6b4b64d745977f148606505752bf97157a4
BatirDaniel <daniel.batir2003@gmail.com>
2023-08-15 09:09:21 +0300

    views: added view for dashboard that contains analytics and many user aids

---
+++ Libro/Libro.Presentation/Controllers/Analytics/Analytics.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libro.Presentation.Controllers.Analytics
+{
+    public class Analytics : Controller
+    {
+        private readonly IMediator _mediator;
+        public Analytics(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Route("dashboard")]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
