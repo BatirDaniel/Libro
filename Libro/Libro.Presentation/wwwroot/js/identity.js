0e32a6669ebc2fcf6501169575cb5e843d3d4b94
BatirDaniel <daniel.batir2003@gmail.com>
2023-08-15 09:30:51 +0300

    added validation for register form with jquery-validation

--- Libro/Libro.Presentation/wwwroot/js/identity.js
+++ Libro/Libro.Presentation/wwwroot/js/identity.js
@@ -17,6 +17,6 @@ $(document).ready(function () {
     });
 });
 
-$(document).ready(function () {
-    $("#registerForm").validate()
+$(document).ready(() => {
+    $("#registerForm").validate();
 });
