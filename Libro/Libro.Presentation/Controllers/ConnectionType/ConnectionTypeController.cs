f1fdcd6cffaaa6c4583b625b440af907f005d2da
BatirDaniel <daniel.batir2003@gmail.com>
2023-09-01 18:31:36 +0300

    new connection type controller which contains a method that returns the list of connection types

---
+++ Libro/Libro.Presentation/Controllers/ConnectionType/ConnectionTypeController.cs
@@ -0,0 +1,41 @@
+﻿using Libro.Business.Libra.DTOs.ConnectionTypesDTOs;
+using Libro.DataAccess.Contracts;
+using Libro.DataAccess.Data;
+using Libro.Infrastructure.Services.ToastService;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libro.Presentation.Controllers.ConnectionType
+{
+    [Authorize]
+    public class ConnectionTypeController : LibroController
+    {
+        private new IUnitOfWork _unitOfWork;
+        public ConnectionTypeController(
+            IToastService toastService,
+            IUnitOfWork unitOfWork,
+            ApplicationDbContext context,
+            IMediator mediator)
+            : base(toastService, unitOfWork, context, mediator)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        //GET: /ConnectionTypes/GetConnectionTypes
+        [HttpGet("/ConnectionTypes/GetConnectionTypes")]
+        public async Task<IActionResult> GetConnectionTypes()
+        {
+            var result = (await _unitOfWork.ConnectionTypes.FindAll<ConnectionTypeDTO>(
+                skip: 0,
+                take: int.MaxValue,
+                select: x => new ConnectionTypeDTO
+                {
+                    Id = x.Id,
+                    Type = x.ConnectionType
+                })).ToList();
+
+            return Ok(result);
+        }
+    }
+}
