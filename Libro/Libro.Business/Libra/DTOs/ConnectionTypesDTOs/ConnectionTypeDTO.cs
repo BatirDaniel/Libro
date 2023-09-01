eb1c7aa0575a547cf07bca87700b824e65127590
BatirDaniel <daniel.batir2003@gmail.com>
2023-09-01 18:29:01 +0300

    dto: new connection type dto

---
+++ Libro/Libro.Business/Libra/DTOs/ConnectionTypesDTOs/ConnectionTypeDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.Business.Libra.DTOs.ConnectionTypesDTOs
+{
+    public class ConnectionTypeDTO
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+    }
+}
