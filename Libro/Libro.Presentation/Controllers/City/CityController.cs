5ba9aa3b70f4615eb8d86558e36f8aeda64a5bbc
BatirDaniel <daniel.batir2003@gmail.com>
2023-09-01 18:30:50 +0300

    new city controller which contains a method that returns the list of cities

---
+++ Libro/Libro.Presentation/Controllers/City/CityController.cs
@@ -0,0 +1,41 @@
+﻿using Libro.Business.Libra.DTOs.CityDTOs;
+using Libro.DataAccess.Contracts;
+using Libro.DataAccess.Data;
+using Libro.Infrastructure.Services.ToastService;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libro.Presentation.Controllers.City
+{
+    [Authorize]
+    public class CityController : LibroController
+    {
+        private new IUnitOfWork _unitOfWork;
+        public CityController(
+            IMediator mediator,
+            IToastService toastService,
+            IUnitOfWork unitOfWork,
+            ApplicationDbContext context) 
+            : base(toastService, unitOfWork, context, mediator)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        //GET: /Cities/GetCities
+        [HttpGet("/Cities/GetCities")]
+        public async Task<IActionResult> GetCities()
+        {
+            var result = (await _unitOfWork.Cities.FindAll<CityDTO>(
+                skip: 0,
+                take: int.MaxValue,
+                select: x => new CityDTO
+                {
+                    Id = x.Id,
+                    Name = x.CityName
+                })).ToList();
+
+            return Ok(result);
+        }
+    }
+}
