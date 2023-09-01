8579ca467f1d99938d8d1b9900438c9a9f60dddc
BatirDaniel <daniel.batir2003@gmail.com>
2023-09-01 18:28:26 +0300

    dto: new city object

---
+++ Libro/Libro.Business/Libra/DTOs/CityDTOs/CityDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.Business.Libra.DTOs.CityDTOs
+{
+    public class CityDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
